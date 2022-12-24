aaa9a1bf202a5f35c763793b7cf138ae0e64c262
johelder <johelder.araujo@pede.ai>
2022-12-24 11:30:40 -0300

    feat: create app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    background: '#ffffff',
+    primary: '#FF5700',
+    dark: '#333333',
+    gray_400: '#888888',
+    attention: '#e83f5b',
+    success: '#12a454',
+  },
+
+  fonts: {
+    primary: {
+      regular: 'IBMPlexSans-Regular',
+      medium: 'IBMPlexSans-Medium',
+      semiBold: 'IBMPlexSans-SemiBold',
+    },
+  },
+};
