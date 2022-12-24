b47ce4325f8027353722b39075ee4d2ec00e0fc5
johelder <johelder.araujo@pede.ai>
2022-12-24 16:10:55 -0300

    feat: adding tab bar icons

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -2,15 +2,63 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AppStackRoutes } from './app.stack.routes';
 
+import * as Phosphor from 'phosphor-react-native';
+import { useTheme } from 'styled-components';
+
 const Tab = createBottomTabNavigator();
 
 export const AppTabsRoutes = () => {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="best" component={AppStackRoutes} />
-      <Tab.Screen name="new" component={AppStackRoutes} />
-      <Tab.Screen name="hot" component={AppStackRoutes} />
-      <Tab.Screen name="controversial" component={AppStackRoutes} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.gray_400,
+      }}
+    >
+      <Tab.Screen
+        name="Best"
+        component={AppStackRoutes}
+        initialParams={{ slug: 'best' }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Phosphor.Rocket color={color} size={32} weight="regular" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="New"
+        component={AppStackRoutes}
+        initialParams={{ slug: 'new' }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Phosphor.Sparkle color={color} size={32} weight="regular" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Hot"
+        component={AppStackRoutes}
+        initialParams={{ slug: 'hot' }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Phosphor.Fire color={color} size={32} weight="regular" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Controversial"
+        component={AppStackRoutes}
+        initialParams={{ slug: 'controversial' }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Phosphor.Sword color={color} size={32} weight="regular" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
