c96cfaad12012673f8fa0d1a112aad0cd08e4eb0
johelder <johelder.araujo@pede.ai>
2022-12-24 13:07:38 -0300

    feat: adding app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
+import { Routes } from './routes';
+
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles/theme';
 
-import { Home } from './pages/Home';
-
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Routes />
     </ThemeProvider>
   );
 };
