675baafa0b169c1c2dfa2e13ad30ef0202b58278
johelder <johelder.araujo@pede.ai>
2022-12-24 16:09:48 -0300

    feat: passing slug as initial param to the stack routes

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TRootStackParamList, TStackProps } from './types';
+
 import { Home } from '../pages/Home';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<TRootStackParamList>();
 
-export const AppStackRoutes = () => {
+export const AppStackRoutes = ({ route }: TStackProps) => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+        initialParams={route.params}
+      />
     </Stack.Navigator>
   );
 };
