e4fb24664120f9dc30e1b9a2904fa41e8309642b
johelder <johelder.araujo@pede.ai>
2022-12-24 13:13:32 -0300

    refactor: patter hex color lowercase

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
     background: '#ffffff',
-    primary: '#FF5700',
+    primary: '#ff5700',
     dark: '#333333',
     gray_400: '#888888',
     attention: '#e83f5b',
