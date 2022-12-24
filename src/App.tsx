91007826b66472ea181d683b83c88d5da5900118
johelder <johelder.araujo@pede.ai>
2022-12-24 10:49:25 -0300

    feat: create src folder

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{ fontFamily: 'IBMPlexSans-SemiBold' }}>Hello World!</Text>
+    </SafeAreaView>
+  );
+};
