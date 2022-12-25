24657f3d79d22f5a9060d36ce91262b61233ff50
johelder <johelder.araujo@pede.ai>
2022-12-25 01:35:38 -0300

    feat: adding subreddit provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { Routes } from './routes';
 
 import { ThemeProvider } from 'styled-components';
+import { SubRedditProvider } from './hooks/useSubreddit';
 
 import { theme } from './styles/theme';
 
 export const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <SubRedditProvider>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </SubRedditProvider>
   );
 };
